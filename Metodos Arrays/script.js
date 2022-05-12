d190b59d3a1f3295d7c63ac46a04f83968706ece
Marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2022-05-12 13:10:01 +0100

    Firts commit

---
+++ Metodos Arrays/script.js
@@ -0,0 +1,40 @@
+let popTest = () =>{
+    let nombres = ['pedro','paco','maria'];
+
+    document.write('<b>Original: </b>' + nombres + '<br>');
+
+    let resultado = nombres.pop(); // Remueve el ultimo y lo devuele
+
+    document.write('<b>POP: </b>' + resultado + '<br>');
+
+    document.write('<b>Resultado: </b>' + nombres);
+
+}
+
+
+let shiftTest = () =>{
+    let nombres = ['pedro','paco','maria'];
+
+    document.write('<b>Original: </b>' + nombres + '<br>');
+
+    let resultado = nombres.shift(); // Remueve el primero y lo devuele
+
+    document.write('<b>Shift: </b>' + resultado + '<br>');
+
+    document.write('<b>Resultado: </b>' + nombres);
+
+}
+
+
+let pushTest = () =>{
+    let nombres = ['pedro','paco','maria'];
+
+    document.write('<b>Original: </b>' + nombres + '<br>');
+
+    let resultado = nombres.shift(); // Remueve el primero y lo devuele
+
+    document.write('<b>Shift: </b>' + resultado + '<br>');
+
+    document.write('<b>Resultado: </b>' + nombres);
+
+}
